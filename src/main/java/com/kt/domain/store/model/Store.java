f1845b4f5721b0c139644f111fffe8583fce2111
l-lyun <kimdohyun032@gmail.com>
2025-11-24 20:11:28 +0900

    feat: store entity 구현

---
+++ src/main/java/com/kt/domain/store/model/Store.java
@@ -0,0 +1,26 @@
+package com.kt.domain.store.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kt.domain.product.model.Product;
+import com.kt.global.common.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store extends BaseEntity {
+	@NotNull
+	private String name;
+	@NotNull
+	private String description;
+	@OneToMany(mappedBy = "store")
+	private List<Product> productList = new ArrayList<>();
+}
