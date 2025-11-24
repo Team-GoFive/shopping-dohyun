6a6f5cabc9bc2541d1b34d75df414405887bd22f
l-lyun <kimdohyun032@gmail.com>
2025-11-24 20:16:00 +0900

    feat: seller store 엔티티 구현 및 매핑

--- src/main/java/com/kt/domain/seller/model/Seller.java
+++ src/main/java/com/kt/domain/seller/model/Seller.java
@@ -1,7 +1,32 @@
 package com.kt.domain.seller.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kt.domain.store.model.Store;
 import com.kt.global.common.BaseEntity;
 
-public class Seller extends BaseEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Seller extends BaseEntity {
+	@NotNull
+	private String name;
+	@NotNull
+	private String mobile;
+	@NotNull
+	@Email
+	private String email;
+	@OneToMany(mappedBy = "seller")
+	private List<Store> storeList = new ArrayList<>();
+
+	// TODO: 사업자 등록 번호
 }
--- src/main/java/com/kt/domain/store/model/Store.java
+++ src/main/java/com/kt/domain/store/model/Store.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.kt.domain.product.model.Product;
+import com.kt.domain.seller.model.Seller;
 import com.kt.global.common.BaseEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -23,4 +26,7 @@ public class Store extends BaseEntity {
 	private String description;
 	@OneToMany(mappedBy = "store")
 	private List<Product> productList = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
 }
