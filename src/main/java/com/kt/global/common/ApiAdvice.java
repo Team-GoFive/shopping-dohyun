22cd4c2d9545f2f44a46ff194a1fe1c18f2cb980
l-lyun <kimdohyun032@gmail.com>
2025-11-25 20:33:59 +0900

    feat: ApiAdvice 작성

---
+++ src/main/java/com/kt/global/common/ApiAdvice.java
@@ -0,0 +1,56 @@
+package com.kt.global.common;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
+@RestControllerAdvice
+public class ApiAdvice {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse.ErrorData> internalServerError(Exception e) {
+		e.printStackTrace();
+		// 서버에러입니다.
+		return ErrorResponse.error(
+			HttpStatus.INTERNAL_SERVER_ERROR,
+			"서버 에러입니다. 백엔드 팀에 문의해주세요",
+			"error"
+		);
+	}
+
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ErrorResponse.ErrorData> customException(CustomException e) {
+		return ErrorResponse.error(
+			e.getErrorCode().getHttpStatus(),
+			e.getErrorCode().getMessage(),
+			e.getErrorCode().getCode()
+		);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse.ErrorData> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+		e.printStackTrace();
+		var details = Arrays.toString(e.getDetailMessageArguments());
+		var message = details.split(
+			",",
+			2
+		)[1].replace(
+			"]",
+			""
+		).trim();
+
+		return ErrorResponse.error(
+			HttpStatus.BAD_REQUEST,
+			message,
+			"A01"
+		);
+	}
+
+}
