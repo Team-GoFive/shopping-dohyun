f99899d4118a86f1fe6eedd52a8a60d80c4162b5
l-lyun <kimdohyun032@gmail.com>
2025-11-25 20:33:50 +0900

    feat: ErrorReponse 구현

---
+++ src/main/java/com/kt/global/common/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.kt.global.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+	private HttpStatus status;
+	private String message;
+
+	public static ResponseEntity<ErrorData> error(HttpStatus status, String message, String code) {
+		return ResponseEntity.status(status).body(ErrorData.of(
+			status.series().name(),
+			message,
+			code
+		));
+	}
+
+	@Getter
+	@AllArgsConstructor
+	public static class ErrorData {
+		private String status;
+		private String code;
+		private String message;
+
+		public static ErrorData of(String status, String message, String code) {
+			return new ErrorData(
+				status,
+				code,
+				message
+			);
+		}
+	}
+}
