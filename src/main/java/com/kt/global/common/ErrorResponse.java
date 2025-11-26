747e7ed0acb9715550e97ed955c299fa18e781bd
l-lyun <kimdohyun032@gmail.com>
2025-11-26 12:04:57 +0900

    fix: ErrorResponse record로 변경

--- src/main/java/com/kt/global/common/ErrorResponse.java
+++ src/main/java/com/kt/global/common/ErrorResponse.java
@@ -3,22 +3,13 @@ package com.kt.global.common;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class ErrorResponse {
-	private final HttpStatus status;
-	private final String code;
-	private final String message;
-
+public record ErrorResponse(HttpStatus status, String message, String code) {
 	public static ResponseEntity<ErrorResponse> of(HttpStatus status, String message, String code) {
 		return ResponseEntity.status(status).body(
 			new ErrorResponse(
 				status,
-				code,
-				message
+				message,
+				code
 			)
 		);
 	}
