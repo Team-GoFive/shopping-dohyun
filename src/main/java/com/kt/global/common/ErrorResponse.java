11f82c1189d3e7c6a559b59e6496727fb823ce51
l-lyun <kimdohyun032@gmail.com>
2025-11-26 12:13:46 +0900

    fix: ResponseEntity body status 제거

--- src/main/java/com/kt/global/common/ErrorResponse.java
+++ src/main/java/com/kt/global/common/ErrorResponse.java
@@ -3,11 +3,10 @@ package com.kt.global.common;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public record ErrorResponse(HttpStatus status, String message, String code) {
+public record ErrorResponse(String message, String code) {
 	public static ResponseEntity<ErrorResponse> of(HttpStatus status, String message, String code) {
 		return ResponseEntity.status(status).body(
 			new ErrorResponse(
-				status,
 				message,
 				code
 			)
